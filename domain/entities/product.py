943e65e3985a1269c05367ee0bc25f57dec266c0
Joel Razuri <jrazuri98@hotmail.com>
2025-04-22 22:06:08 -0300

    clase Producto

--- domain/entities/product.py
+++ domain/entities/product.py
@@ -1,1 +1,18 @@
+# Clase Producto
+class Product():
+    def __init__(self, id:int, name:str, description:str, category:str, stock:int, price:float):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.category = category
+        self.stock = stock
+        self.price = price
 
+    def __str__(self):
+        return f""""
+                Producto: {self.name}
+                Descripción: {self.description}
+                Categoría: {self.category}
+                Stock: {self.stock}
+                Precio: ${self.price}
+                """
