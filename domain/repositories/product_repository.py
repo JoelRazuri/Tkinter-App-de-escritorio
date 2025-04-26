ae98895fdc4be6c581e22150708a2971d21fedb1
Joel Razuri <jrazuri98@hotmail.com>
2025-04-26 19:48:59 -0300

    clase ProductRepository

---
+++ domain/repositories/product_repository.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+from typing import List
+from entities.product import Product
+
+
+class ProductRepository(ABC):
+    @abstractmethod
+    def add_product(self, product: Product) -> Product:
+        """  Crea un producto y devuelve su entidad. """
+        ...
+    
+    @abstractmethod
+    def get_all_products(self) -> List[Product]:
+        """  Devuelve todos los productos existentes. """
+        ...
+
+    @abstractmethod
+    def get_product_by_id(self, product_id: int) -> Product:
+        """  Busca y devuelve un producto según su ID. """
+        ...
+
+    @abstractmethod
+    def update_product(self, product_id: int, product_new: Product) -> Product:
+        """  Actualiza un producto según su ID y devuelve la versión actualizada. """
+        ...
+
+    @abstractmethod
+    def delete_product(self, product_id: int) -> None:
+        """  Elimina un producto según su ID. """
+        ...
